dd0384c07ffe27b675db349c4f6a6b02510f9a9a
Marvin Mpanga <bussiness.mavn@gmail.com>
2025-11-10 18:24:10 +0300

    Uncomment and complete _formatTimeAgo function

--- lib/screens/health_worker/hw_chat_list_screen.dart
+++ lib/screens/health_worker/hw_chat_list_screen.dart
@@ -260,14 +260,20 @@ class _HWChatCard extends StatelessWidget {
     );
   }
 
-  // String _formatTimeAgo(DateTime dateTime) {
-  //   final now = DateTime.now();
-  //   final difference = now.difference(dateTime);
+  String _formatTimeAgo(DateTime dateTime) {
+    final now = DateTime.now();
+    final difference = now.difference(dateTime);
 
-  //   if (difference.inMinutes < 1) {
-  //     return 'Just now';
-  //   } else if (difference.inHours < 1) {
-  //     return '${difference.inMinutes}m ago';
-  //   } else if (difference.inHours < 24) {
-  //     return '${difference.inHours}h ago';
-  //   } else if (difference.inDays
+    if (difference.inMinutes < 1) {
+      return 'Just now';
+    } else if (difference.inHours < 1) {
+      return '${difference.inMinutes}m ago';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours}h ago';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays}d ago';
+    } else {
+      return DateFormat('MMM d').format(dateTime);
+    }
+  }
+}
